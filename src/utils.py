f96b5e9bd08b761f8c3f0c93777496343afc18ee
Fakhr-eldeen <fakhrldeen12@gmail.com>
2025-12-17 02:30:25 +0200

    model trainer

--- src/utils.py
+++ src/utils.py
@@ -4,7 +4,7 @@ import pandas as pd
 import numpy as np
 from src.exceptions import CustomException
 import dill
-
+from sklearn.metrics import r2_score
 
 def save_object(file_path, obj):
     try:
@@ -20,3 +20,22 @@ def load_object(file_path):
         return dill.load(file_path)
     except Exception as e:
         raise e
+    
+def evaluate_model(X_train, y_train, X_test, y_test, models):
+    try:
+        report = {}
+        for i in range(len(list(models))):
+            model = list(models.values())[i]
+            # Train model
+            model.fit(X_train, y_train)
+            y_test_pred = model.predict(X_train)
+            # Predict Testing data
+            y_test_pred = model.predict(X_test)
+
+            # Get r2 score for the model
+            test_model_score = r2_score(y_test, y_test_pred)
+
+            report[list(models.keys())[i]] = test_model_score
+        return report
+    except Exception as e:
+        raise CustomException(e, sys)
