eb641ed239a79c9ed62c2ea0bed3741503736bb6
Fakhr-eldeen <fakhrldeen12@gmail.com>
2025-12-17 03:06:14 +0200

    hyper paramiter training

--- src/components/model_trainer.py
+++ src/components/model_trainer.py
@@ -43,7 +43,41 @@ class ModelTrainer:
                 "CatBoosting Regressor": CatBoostRegressor(),
                 "AdaBoost Regressor": AdaBoostRegressor()
             }
-            model_report:dict=evaluate_model(X_train=X_train, y_train=y_train, X_test=X_test, y_test=y_test, models=models)
+            params = {
+                "Decision Tree": {
+                    'criterion':['squared_error', 'friedman_mse', 'absolute_error', 'poisson'],
+                    'splitter':['best','random'],
+                    'max_features':['sqrt','log2']
+                },
+                "Random Forest": {
+                    'n_estimators':[8,16,32,64,128,256],
+                    'max_depth':[5,10,15,20,25,30],
+                    'min_samples_split':[2,5,10],
+                },    
+                "Gradient Boosting": {
+                    'learning_rate':[.1,.01,.05,.001],
+                    "subsample":[0.6,0.7,0.75,0.8,0.85,0.9],
+                    'n_estimators':[8,16,32,64,128,256]
+                },
+                "Linear Regression": {
+                },
+
+                "K-Neighbours Regressor": {
+                    'n_neighbors':[3,5,7,9],
+                    'weights':['uniform','distance'],
+                    'algorithm':['auto','ball_tree','kd_tree']
+                },
+                "XGB Regressor": {
+                    'learning_rate':[.1,.01,.05,.001],
+                    'n_estimators':[8,16,32,64,128,256]
+                },
+
+                "AdaBoost Regressor": {
+                    'learning_rate':[.1,.01,.05,.001],
+                    'n_estimators':[8,16,32,64,128,256]
+                }
+            }
+            model_report:dict=evaluate_model(X_train=X_train, y_train=y_train, X_test=X_test, y_test=y_test, models=models,params=params)
             best_model_score = max(sorted(model_report.values()))
             best_model_name = list(model_report.keys())[
                 list(model_report.values()).index(best_model_score)
@@ -54,7 +88,7 @@ class ModelTrainer:
             if best_model_score < 0.6:
                 raise CustomException("No best model found with R2 score above threshold")
             logging.info(f"Best model found: {best_model_name} with R2 score: {best_model_score}")
-            
+             
             save_object(
                 file_path=self.model_trainer_config.trained_model_file_path,
                 obj=best_model
--- src/utils.py
+++ src/utils.py
@@ -5,7 +5,7 @@ import numpy as np
 from src.exceptions import CustomException
 import dill
 from sklearn.metrics import r2_score
-
+from sklearn.model_selection import GridSearchCV 
 def save_object(file_path, obj):
     try:
         dir_path = os.path.dirname(file_path)
@@ -21,21 +21,47 @@ def load_object(file_path):
     except Exception as e:
         raise e
     
-def evaluate_model(X_train, y_train, X_test, y_test, models):
+def evaluate_model(X_train, y_train, X_test, y_test, models,params):
     try:
         report = {}
-        for i in range(len(list(models))):
-            model = list(models.values())[i]
-            # Train model
-            model.fit(X_train, y_train)
-            y_test_pred = model.predict(X_train)
-            # Predict Testing data
+        for model_name in models:
+            model = models[model_name]
+            
+            # Get hyperparameters for this model
+            para = params.get(model_name, {})
+            
+            # Special handling for CatBoost - skip GridSearchCV
+            if "CatBoost" in model_name or "CatBoosting" in model_name:
+                # Train CatBoost with default or custom params directly
+                if para:
+                    model.set_params(**para)
+                model.fit(X_train, y_train, verbose=False)
+            
+            elif para:
+                # For other models, use GridSearchCV
+                gs = GridSearchCV(
+                    estimator=model,
+                    param_grid=para,
+                    cv=3,
+                    n_jobs=-1,
+                    verbose=0
+                )
+                gs.fit(X_train, y_train)
+                model.set_params(**gs.best_params_)
+                model.fit(X_train, y_train)
+            else:
+                # No hyperparameters, train directly
+                model.fit(X_train, y_train)
+            
+            # Make predictions
+            y_train_pred = model.predict(X_train)
             y_test_pred = model.predict(X_test)
-
-            # Get r2 score for the model
+            
+            # Calculate R2 scores
+            train_model_score = r2_score(y_train, y_train_pred)
             test_model_score = r2_score(y_test, y_test_pred)
-
-            report[list(models.keys())[i]] = test_model_score
+            
+            report[model_name] = test_model_score
         return report
     except Exception as e:
         raise CustomException(e, sys)
